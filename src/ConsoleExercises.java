bba32ac08c9e50825ed182a02b441764f9c6f40f
Kristen Wilson <wilson.kristen614@gmail.com>
2021-09-03 15:35:35 -0500

    do refactor later

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,8 +1,43 @@
-
-
-
-
-
+import java.util.Scanner;
 
 public class ConsoleExercises {
+    public static void main(String[] args) {
+      //  double pi = 3.14159;
+     //   System.out.format("The value of pi is approximately %.2F", pi);
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n Please enter an integer ");
+        int usrInput = scanner.nextInt();
+       System.out.println(usrInput);
+
+       System.out.println("Enter three words: ");
+       String word1 = scanner.next();
+       String word2 = scanner.next();
+       String word3 = scanner.next();
+
+       System.out.printf("Your three words were: %n%s%n%s%n%s. ", word1, word2, word3);
+            String length = scanner
+       
+
+       
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
