25cc7acd642179f510d12a40870ae5c16997bdfe
Kristen Wilson <wilson.kristen614@gmail.com>
2021-09-08 09:29:34 -0500

    therapist come back to it later

---
+++ src/Therapist.java
@@ -0,0 +1,25 @@
+
+public class Therapist {
+    public static void main(String[] args) {
+
+        String askMe = "how are you?";
+
+        String iSay = "I'm doing fine how are you?"
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
