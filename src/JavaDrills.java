c0c0b0d92e37a0009ebbad1448a5b3626bb4b5b4
kris <wilson.kristen614@gmail.com>
2021-10-21 09:34:27 -0500

    morning warm up

--- src/JavaDrills.java
+++ src/JavaDrills.java
@@ -29,7 +29,37 @@ public class JavaDrills {
     }
       return total;
 }
-
+ //https://commons.apache.org/proper/commons-lang/javadocs/api-2.6/src-html/org/apache/commons/lang/StringUtils.html#line.5259
+    public static String swapCase(String str) {
+                int strLen;
+             if (str == null || (strLen = str.length()) == 0) {
+                        return str;
+                }
+                   StrBuilder buffer = new StrBuilder(strLen);
+        
+                   char ch = 0;
+                   for (int i = 0; i < strLen; i++) {
+                          ch = str.charAt(i);
+                          if (Character.isUpperCase(ch)) {
+                                    ch = Character.toLowerCase(ch);
+                                } else if (Character.isTitleCase(ch)) {
+                                    ch = Character.toLowerCase(ch);
+                                } else if (Character.isLowerCase(ch)) {
+                                    ch = Character.toUpperCase(ch);
+                                }
+                         buffer.append(ch);
+                     }
+            return buffer.toString();
+               }
+    //In `codeup-java-exercises`, in`JavaDrills`, add the following:
+    //Create a public static method, `flipInnerCase`, that takes in a string and returns the string
+    //with the case of all letters reversed EXCEPT for the first and last letters. Assume the input will be three or more characters long.
+    //Example inputs and expected outputs...
+    //  System.out.println(flipInnerCase(“cat”)); // cAt
+    //  System.out.println(flipInnerCase(“CaT”)); // CAT
+    //  System.out.println(flipInnerCase(“caT”)); // cAT
+    //  System.out.println(flipInnerCase(“codeup”)); // cODEUp
+    //  System.out.println(flipInnerCase(“CoDeup”)); // COdEUp
 
     //working progress  https://stackoverflow.com/questions/1729778/how-can-i-invert-the-case-of-a-string-in-java
     public static void main(String[] args) {
@@ -37,5 +67,13 @@ public class JavaDrills {
         System.out.println(JavaDrills.flipOuterCase("Cat"));
 
 
+    }
+
+    private static class StrBuilder {
+        public StrBuilder(int strLen) {
+        }
+
+        public void append(char ch) {
+        }
     }
 }
