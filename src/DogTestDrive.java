7923fa6c295efa6dcf520e7a1bc5cdedf729cf83
Kristen Wilson <Kristen@Kristens-Air.attlocal.net>
2022-02-28 17:39:21 -0600

    simple exercises

---
+++ src/DogTestDrive.java
@@ -0,0 +1,10 @@
+public class DogTestDrive {
+    public static void main(String[] args) {
+        //make a new Dog object
+        Dog d = new Dog();
+        //use the dot operator to set the size of the Dog
+        d.size = 40;
+        // and the call its bark() method
+        d.bark();
+    }
+}
